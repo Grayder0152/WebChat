dc0e58433191230784e2f4f05773c436654be6b6
Sergey Derevianko <derevankosergej0@gmail.com>
2021-07-03 17:46:25 +0300

    Create apps 'user' and 'chat'

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2*hah9x0j+tv7d_^^sqe-g&oa5-i9ckg79b9=1i=#+755mtmiz'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -14,6 +14,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'user',
+    'chat',
 ]
 
 MIDDLEWARE = [
