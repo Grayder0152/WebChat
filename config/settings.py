cbb6fd39df5bf6bc5702e4f2c0208200929adce3
Sergey Derevianko <derevankosergej0@gmail.com>
2021-07-30 14:42:07 +0300

    Change db on PostgreSQL and add requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -51,8 +51,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat_db',
+        'USER': 'chat_admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
